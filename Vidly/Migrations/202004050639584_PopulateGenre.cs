1c26884a0e42de3e9828c61f2d627afdccc88b8b
yankzsoe <yankzsoe@gmail.com>
2020-04-05 21:52:00 +0700

    FixingGenreIdInMovies, PopulateGenre

---
+++ Vidly/Migrations/202004050639584_PopulateGenre.cs
@@ -0,0 +1,16 @@
+namespace Vidly.Migrations {
+    using System;
+    using System.Data.Entity.Migrations;
+
+    public partial class PopulateGenre : DbMigration {
+        public override void Up() {
+            Sql("INSERT INTO GENRES (Name) VALUES ('Action')");
+            Sql("INSERT INTO GENRES (Name) VALUES ('Comedy')");
+            Sql("INSERT INTO GENRES (Name) VALUES ('Family')");
+            Sql("INSERT INTO GENRES (Name) VALUES ('Romance')");
+        }
+
+        public override void Down() {
+        }
+    }
+}
