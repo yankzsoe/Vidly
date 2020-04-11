b1abc1529e06b1650fa2c72c186ccf4c8014ec97
yankzsoe <yankzsoe@gmail.com>
2020-04-12 06:51:18 +0700

    ExternalLoginConfirmationViewModel and RegisterViewModel to separate file.

---
+++ Vidly/Models/ExternalLoginConfirmationViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models {
+    public class ExternalLoginConfirmationViewModel {
+        [Required]
+        [Display(Name = "Driving License")]
+        public string DrivingLicense { get; set; }
+
+        [Required, StringLength(50)]
+        [DataType(DataType.PhoneNumber)]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
